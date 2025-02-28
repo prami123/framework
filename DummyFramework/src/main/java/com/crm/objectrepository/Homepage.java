ecfdccd72f1e0bf97bd910dc8767b55e03ae617c
Prave <Prave@HP_Prameela>
2025-02-28 10:09:20 +0530

    adding ele4 to homepage

--- DummyFramework/src/main/java/com/crm/objectrepository/Homepage.java
+++ DummyFramework/src/main/java/com/crm/objectrepository/Homepage.java
@@ -18,6 +18,9 @@ public class Homepage {
 	private WebElement ele2; 
 	
 	@FindBy(xpath ="//a[text()='Products']")
+	private WebElement ele4;
+	
+	@FindBy(xpath ="//a[text()='Products']")
 	private WebElement prodlink;
 	
 	@FindBy(xpath ="(//a[text()='Organizations'])[1]")
